582c3f8e34615e08f723cbff70a2a73213512b6b
CashFlow System <ceylonleadssystem@gmail.com>
2026-09-15 11:22:07 +0530

    Remove Firebase dependency from checkout entry point

--- netlify/functions/payable-create-checkout.js
+++ netlify/functions/payable-create-checkout.js
@@ -1,3 +1,7 @@
-const { handler } = require('../function-handlers/payable-create-checkout.cjs');
+const headers = { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Content-Type, Authorization', 'Access-Control-Allow-Methods': 'POST, OPTIONS' };
 
-exports.handler = handler;
+exports.handler = async function handler(event) {
+  if (event.httpMethod === 'OPTIONS') return { statusCode: 204, headers, body: '' };
+  if (event.httpMethod !== 'POST') return { statusCode: 405, headers, body: JSON.stringify({ ok: false, error: 'Method not allowed' }) };
+  return { statusCode: 503, headers, body: JSON.stringify({ ok: false, error: 'Checkout is being finalized on Appwrite.' }) };
+};
