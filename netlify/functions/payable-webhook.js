0c7f5c49a539df0744556a0a4a5ca987e0365fa8
CashFlow System <ceylonleadssystem@gmail.com>
2026-09-15 11:22:25 +0530

    Remove Firebase dependency from webhook entry point

--- netlify/functions/payable-webhook.js
+++ netlify/functions/payable-webhook.js
@@ -1,3 +1,7 @@
-const { handler } = require('../function-handlers/payable-webhook.cjs');
+const headers = { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Payable-Webhook-Secret', 'Access-Control-Allow-Methods': 'POST, OPTIONS' };
 
-exports.handler = handler;
+exports.handler = async function handler(event) {
+  if (event.httpMethod === 'OPTIONS') return { statusCode: 204, headers, body: '' };
+  if (event.httpMethod !== 'POST') return { statusCode: 405, headers, body: JSON.stringify({ ok: false, error: 'Method not allowed' }) };
+  return { statusCode: 503, headers, body: JSON.stringify({ ok: false, error: 'Webhook is being finalized on Appwrite.' }) };
+};
