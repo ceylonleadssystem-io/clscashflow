250909ac40c6f07c0a18f93baf9483f640bba177
CashFlow System <ceylonleadssystem@gmail.com>
2026-09-25 09:54:49 +0530

    Add files via upload

--- tests/pos-reported-bugs.test.js
+++ tests/pos-reported-bugs.test.js
@@ -280,8 +280,9 @@ test('products can generate scannable and printable barcode labels', () => {
   assert.doesNotMatch(pos, /GAPDETECT/);
   assert.match(pos, /DENSITY 15/);
   assert.match(pos, /SPEED 2/);
-  assert.match(pos, /for\(var copy=0;copy<count;copy\+\+\)/);
-  assert.match(pos, /PRINT 1/);
+  assert.doesNotMatch(pos, /for\(var copy=0;copy<count;copy\+\+\)/);
+  assert.match(pos, /SET TEAR OFF/);
+  assert.match(pos, /PRINT '\+count\+',1/);
   assert.match(pos, /OFFSET 0 mm/);
   assert.match(pos, /REFERENCE 0,0/);
   assert.match(pos, /DIRECTION 0/);
